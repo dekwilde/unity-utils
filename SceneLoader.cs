a67862b0c916fb6574746e06f67f0e666424d8d9
Henrique Derosa <contato.h@gmail.com>
2019-04-05 23:22:22 -0300

    Add scene loader singleton

---
+++ SceneLoader.cs
@@ -0,0 +1,26 @@
+/**
+    SceneLoader.cs
+    Call it from a MonoBehaviour 
+        `SceneLoader.instance.LoadScene(name or id)` to load another scene
+ */
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader {
+
+     private static SceneLoader instance; 
+     public static SceneLoader getInstance() {
+          if (instance == null) {
+               instance = new SceneLoader();
+          }
+          return instance;
+     }
+
+     public void LoadScene(string sceneName) {
+          SceneManager.LoadScene(sceneName);
+     }
+
+     public void LoadScene(int buildIndex) {
+          SceneManager.LoadScene(buildIndex);
+     }
+}
