4c43a6c7a6eb3aaf38618811bd0459b56491bd45
DekWilde <dekwilde@gmail.com>
2023-06-22 15:52:04 -0300

    fps

--- FPSDisplay.cs
+++ FPSDisplay.cs
@@ -1,28 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class FPSDisplay : MonoBehaviour
 {
 	float deltaTime = 0.0f;
+	public Text text;
 
 	void Update()
 	{
 		deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
-	}
-
-	void OnGUI()
-	{
-		int w = Screen.width, h = Screen.height;
-
-		GUIStyle style = new GUIStyle();
-
-		Rect rect = new Rect(Screen.width/4, 10f, w, h * 2 / 100);
-		style.alignment = TextAnchor.UpperLeft;
-		style.fontSize = h * 5 / 100;
-		style.normal.textColor = new Color (1.0f, 1.0f, 1.0f, 1.0f);
 		float msec = deltaTime * 1000.0f;
 		float fps = 1.0f / deltaTime;
-		string text = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
-		GUI.Label(rect, text, style);
+		string str = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
+		text.text = str;
 	}
 }
